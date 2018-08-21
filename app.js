0de46b697ecf89ffff36656ca72ec26ab7801b1b
Ankit Maewall <ankit.maewall@gmail.com>
2018-08-20 22:39:35 -0400

    Basic Outline and setup of Environment

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var chalk = require('chalk');
+var debug = require('debug')('app');
+var morgan = require('morgan');
+var path = require('path');
+
+var app = express();
+
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname,'/public/')))
+app.use('/css',express.static(path.join(__dirname,'/node_modules/bootstrap/dist/css')))
+app.use('/js',express.static(path.join(__dirname,'/node_modules/bootstrap/dist/js')))
+app.use('/js',express.static(path.join(__dirname,'/node_modules/jquery/dist/')))
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname,'views/index.html'));
+})
+
+app.listen(3000, function(){
+    console.log(`Listening on port ${chalk.green('3000')}`);
+});
