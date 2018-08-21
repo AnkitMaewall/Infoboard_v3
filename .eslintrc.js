4bbfd8043f148c2df0cd7616d9a2dd6ddb1f9b4b
Ankit Maewall <ankit.maewall@gmail.com>
2018-08-21 11:25:25 -0400

    set up environment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,17 @@
 module.exports = {
-    "extends": "airbnb-base"
+  "extends": "airbnb-base",
+  "rules": {
+    // enable additional rules
+    "indent": ["error", 4],
+    "linebreak-style": ["error", "windows"],
+    //"quotes": ["error", "double"],
+    //"semi": ["error", "always"],
+
+    // override default options for rules from base configurations
+    //"comma-dangle": ["error", "always"],
+    //"no-cond-assign": ["error", "always"],
+
+    // disable rules from base configurations
+    //"no-console": "off",
+  }
 };
--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-var express = require('express');
-var chalk = require('chalk');
-var debug = require('debug')('app');
-var morgan = require('morgan');
-var path = require('path');
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('infoboard_v3');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express();
-
+const app = express();
+const port = process.env.PORT || 3000;
 app.use(morgan('tiny'));
-app.use(express.static(path.join(__dirname,'/public/')))
-app.use('/css',express.static(path.join(__dirname,'/node_modules/bootstrap/dist/css')))
-app.use('/js',express.static(path.join(__dirname,'/node_modules/bootstrap/dist/js')))
-app.use('/js',express.static(path.join(__dirname,'/node_modules/jquery/dist/')))
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
 
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname,'views/index.html'));
-})
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+});
 
-app.listen(3000, function(){
-    console.log(`Listening on port ${chalk.green('3000')}`);
+app.listen(port, () => {
+    debug(`Listening at port ${chalk.green(port)}`);
 });
