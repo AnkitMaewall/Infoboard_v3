03b56d2431a6eba63cfcb706e815a4bafe31fb3d
Ankit Maewall <ankit.maewall@gmail.com>
2018-09-07 17:26:45 -0400

    Added user db to mongo

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const debug = require('debug')('infoboard_v3');
 const morgan = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 // "$Env:DEBUG=\"infoboard_v3\"; ", [Environment]::SetEnvironmentVariable("DEBUG","express:*");
 const app = express();
@@ -16,6 +19,10 @@ app.use((req, res, next) => {
 app.use(morgan('tiny'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({ secret: 'request' }));
+
+require('./src/config/passport.js')(app);
 
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
@@ -23,7 +30,7 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
- 
+
 const nav = [{ link: '/home', title: 'Home' },
   { link: '/security', title: 'Security' },
   { link: '/maintanence', title: 'Maintanence' },
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,13 +1,51 @@
 const express = require('express');
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
 const debug = require('debug');
+const passport= require('passport');
 
-const authRouter = express.Router()
+const authRouter = express.Router();
 
-function router() {
+function router(nav) {
   authRouter.route('/signUp')
     .post((req, res) => {
-      res.json(req.body);
+      const { username, password } = req.body;
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'maintanenceApp';
+
+      (async function addUser() {
+        let client;
+        try {
+          client = await MongoClient.connect(url);
+          debug('Connected correctly to server');
+
+          const db = client.db(dbName);
+
+          const col = db.collection('users');
+          const user = { username, password };
+          const results = await col.insertOne(user);
+          req.login(results.ops[0], () => {
+            res.redirect('/auth/profile');
+          });
+        } catch (err) {
+          console.error(err);
+        }
+      }());
+    });
+
+  authRouter.route('/signin')
+    .get((req, res) => {
+      res.render('signin', {
+        nav,
+        title: 'Sign In'
+      });
+    })
+    .post(passport.authenticate('local', {
+      successRedirect: '/auth/profile',
+      failureRedirect: '/'
+    }));
+  authRouter.route('/profile')
+    .get((req, res) => {
+      res.json(req.user);
     });
   return authRouter;
 }
